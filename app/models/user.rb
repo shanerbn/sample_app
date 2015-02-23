d8b1fc0d4cb9016fc60df9662d204eef8f546e3a
shanerbn <shane@converted.co.uk>
2015-02-23 20:14:35 +0000

    Finished basic User model including secure passwords, tested and bugfixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,23 @@
 class User < ActiveRecord::Base
-  attr_accessor :password, :password_confirmation
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-        format: {with: VALID_EMAIL_REGEX },
-        uniqueness: { case_sensitive: false}
-
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 8 }
 end
+
+# class User < ActiveRecord::Base
+#   attr_accessor :password, :password_confirmation - this caused password/emails to fail testing validation
+#   before_save { self.email = email.downcase }
+#   validates :name, presence: true, length: { maximum: 50 }
+#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#   validates :email, presence: true, length: { maximum: 255 },
+#         format: { with: VALID_EMAIL_REGEX },
+#         uniqueness: { case_sensitive: false}
+#   has_secure_password
+#   validates :password, length: { minimum: 8 }
+# end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,18 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@user.com",
-                password: "foobars", password_confirmation: "foobar")
+    #previous below
+    @user = User.new(name: "Example Name", email: "example@example.com", password: "thisfoobar", password_confirmation: "thisfoobar")
+  end
+
+  # def setup
+  #   @user = User.new(name: "Example User", email: "example@example.com",
+  #                    password: "thisfoobar", password_confirmation: "thisfoobar")
+  # end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
   test "should be valid" do
@@ -32,7 +42,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@example.com USER@foo.com A_User@foo.bar.org
+    valid_addresses = %w[cluser@example.com USER@foo.com A_User@foo.bar.org
                           first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
@@ -41,13 +51,15 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
 
-  test "email address should be unique" do
+  test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
