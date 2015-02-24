8b590a99034600b4c815cce56db69a17666a912e
shanerbn <shane@converted.co.uk>
2015-02-24 00:29:08 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  resources :users
 
   # root 'static_pages#home'
 
